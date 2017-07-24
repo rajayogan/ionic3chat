00ea22e9c38630406f54ccebf611e16e0fc63e3a
rajayogan <rajayogan27@gmail.com>
2017-07-24 21:15:39 +0530

    accepting req

--- src/pages/chats/chats.ts
+++ src/pages/chats/chats.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, Events } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Events, AlertController } from 'ionic-angular';
 import { RequestsProvider } from '../../providers/requests/requests';
 
 /**
@@ -15,30 +15,54 @@ import { RequestsProvider } from '../../providers/requests/requests';
 })
 export class ChatsPage {
   myrequests;
+  myfriends;
   constructor(public navCtrl: NavController, public navParams: NavParams, public requestservice: RequestsProvider,
-              public events: Events) {
+              public events: Events, public alertCtrl: AlertController) {
   }
 
 
   ionViewWillEnter() {
     this.requestservice.getmyrequests();
+    this.requestservice.getmyfriends();
+    this.myfriends = [];
     this.events.subscribe('gotrequests', () => {
       this.myrequests = [];
       this.myrequests = this.requestservice.userdetails;
+    })
+    this.events.subscribe('friends', () => {
+      this.myfriends = [];
+      this.myfriends = this.requestservice.myfriends; 
     })
   }
 
   ionViewDidLeave() {
     this.events.unsubscribe('gotrequests');
+    this.events.unsubscribe('friends');
   }
 
-  
-  
-    
-  
 
   addbuddy() {
     this.navCtrl.push('BuddiesPage');
+  }
+
+  accept(item) {
+    this.requestservice.acceptrequest(item).then(() => {
+      
+      let newalert = this.alertCtrl.create({
+        title: 'Friend added',
+        subTitle: 'Tap on the friend to chat with him',
+        buttons: ['Okay']
+      });
+      newalert.present();
+    })
+  }
+
+  ignore(item) {
+    this.requestservice.deleterequest(item).then(() => {
+
+    }).catch((err) => {
+      alert(err);
+    })
   }
 
 }
--- src/providers/requests/requests.ts
+++ src/providers/requests/requests.ts
@@ -13,7 +13,10 @@ import firebase from 'firebase';
 @Injectable()
 export class RequestsProvider {
   firereq = firebase.database().ref('/requests');
+  firefriends = firebase.database().ref('/friends');
+  
   userdetails;
+  myfriends;
   constructor(public userservice: UserProvider, public events: Events) {
     
   }
@@ -53,7 +56,72 @@ export class RequestsProvider {
       })
 
   })
-}  
+  }  
 
+  acceptrequest(buddy) {
+    var promise = new Promise((resolve, reject) => {
+      this.myfriends = [];
+      this.firefriends.child(firebase.auth().currentUser.uid).push({
+        uid: buddy.uid
+      }).then(() => {
+        this.firefriends.child(buddy.uid).push({
+          uid: firebase.auth().currentUser.uid
+        }).then(() => {
+          this.deleterequest(buddy).then(() => {
+          resolve(true);
+        })
+        
+        }).catch((err) => {
+          reject(err);
+         })
+        }).catch((err) => {
+          reject(err);
+      })
+    })
+    return promise;
+  }
+
+  deleterequest(buddy) {
+    var promise = new Promise((resolve, reject) => {
+     this.firereq.child(firebase.auth().currentUser.uid).orderByChild('sender').equalTo(buddy.uid).once('value', (snapshot) => {
+          let somekey;
+          for (var key in snapshot.val())
+            somekey = key;
+          this.firereq.child(firebase.auth().currentUser.uid).child(somekey).remove().then(() => {
+            resolve(true);
+          })
+         })
+          .then(() => {
+          
+        }).catch((err) => {
+          reject(err);
+        })
+    })
+    return promise; 
+  }
+
+  getmyfriends() {
+    let friendsuid = [];
+    this.firefriends.child(firebase.auth().currentUser.uid).on('value', (snapshot) => {
+      let allfriends = snapshot.val();
+      this.myfriends = [];
+      for (var i in allfriends)
+        friendsuid.push(allfriends[i].uid);
+        
+      this.userservice.getallusers().then((users) => {
+        this.myfriends = [];
+        for (var j in friendsuid)
+          for (var key in users) {
+            if (friendsuid[j] === users[key].uid) {
+              this.myfriends.push(users[key]);
+            }
+          }
+        this.events.publish('friends');
+      }).catch((err) => {
+        alert(err);
+      })
+    
+    })
+  }  
 
 }
